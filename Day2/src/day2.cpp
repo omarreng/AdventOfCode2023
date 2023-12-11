fe2adca17e24b6bedd77ff78c76f898c1c65b454
Oscar <oscar.marrengo@gmail.com>
2023-12-11 19:04:55 +0100

    Adds the .cpp file for the first challenge of day 2

---
+++ Day2/src/day2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "fileio.hpp"
+
+using namespace std;
+
+int main()
+{
+    vector<string> input = readFile("input_day2.txt");
+
+    return 0;
+}
