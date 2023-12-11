9a7f6c1b5fd4ed2f68c6ee59ddd4703531edcea4
Oscar <oscar.marrengo@gmail.com>
2023-12-11 19:40:52 +0100

    Adds utility functions specific to the day 2 challenge
    split_line is supposed to act as a C++ equivalent to the python function
    split(). Given a string, it will split it into subparts according to a
    delimiter character.
    
    process_lines will do the bulk of the work for this challenge. It will
    among other things call split_line on every string in the input data.

--- Day2/src/day2.cpp
+++ Day2/src/day2.cpp
@@ -1,14 +1,44 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "fileio.hpp"
 
 using namespace std;
+
+vector<string> split_line(string str);
+unordered_map<string, int> process_line(string str);
 
 int main()
 {
     vector<string> input = readFile("input_day2.txt");
 
     return 0;
+}
+
+vector<string> split_line(string str, char delimiter)
+{
+    istringstream iss(str);
+    string token;
+    vector<string> result;
+
+    while (std::getline(iss, token, delimiter))
+    {
+        result.push_back(token);
+    }
+
+    return result;
+}
+
+unordered_map<string, int> process_line(string str)
+{
+    unordered_map<string, int> result{
+        {"ID", -1},
+        {"red", 0},
+        {"green", 0},
+        {"blue", 0}};
+
+    return result;
 }
