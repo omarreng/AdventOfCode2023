2c03771ea25cb020270ee552ae75182df927768c
Oscar Manuel Marrero Engström <oscar.marrengo@gmail.com>
2023-12-11 20:46:18 +0100

    Adds functionality to process_line
    process_line now correctly extracts the greatest number of red,
    green and blue gems shown across each game together with the
    corresponding game ID.
    
    Now the rest of the logic to solve the first challenge of day 2
    can be built up from the main function without cluttering it too
    much.

--- Day2/src/day2.cpp
+++ Day2/src/day2.cpp
@@ -15,6 +15,12 @@ int main()
 {
     vector<string> input = readFile("input_day2.txt");
 
+    for (string &s : input)
+    {
+        process_line(s);
+    }
+
+    cout << "Hello from day 2!" << endl;
     return 0;
 }
 
@@ -39,6 +45,19 @@ unordered_map<string, int> process_line(string str)
         {"red", 0},
         {"green", 0},
         {"blue", 0}};
+
+    vector<string> processed_line = split_line(str, ':');
+    result.at("ID") = std::stoi(split_line(processed_line.at(0), ' ').at(1));
+    vector<string> game_instances = split_line(processed_line.at(1), ';');
+    for (string &game : game_instances)
+    {
+        vector<string> game_result = split_line(game, ',');
+        for (string &color : game_result)
+        {
+            vector<string> color_result = split_line(color, ' ');
+            result.at(color_result.at(2)) = std::max(std::stoi(color_result.at(1)), result.at(color_result.at(2)));
+        }
+    }
 
     return result;
 }
