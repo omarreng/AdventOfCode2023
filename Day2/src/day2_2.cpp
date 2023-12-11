4739a48eb3ce82e1562bff6265f12fb95266c1c2
Oscar Manuel Marrero Engström <oscar.marrengo@gmail.com>
2023-12-11 21:09:02 +0100

    Adds solution to second challenge of day 2
    The solution is very similar to the functionality already present
    in the solution for the first challenge of day 2. Since the
    unordered_map already contains the information about the minimum
    amount of gems of each color needed to play each game, the program
    only needed to be modified to compute the power (as specified in
    the challenge) for each game and accumulate for all games played.
    
    The solution computed was submitted and the submission passed.
    Second challenge of day 2 completed and another gold star awarded!

---
+++ Day2/src/day2_2.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include "fileio.hpp"
+
+using namespace std;
+
+vector<string> split_line(string str);
+unordered_map<string, int> process_line(string str);
+
+int main()
+{
+    vector<string> input = readFile("input_day2.txt");
+    int result = 0;
+
+    for (string &s : input)
+    {
+        unordered_map<string, int> game = process_line(s);
+        result += game.at("red") * game.at("green") * game.at("blue");
+    }
+
+    cout << "The result is : " << result << endl;
+    return 0;
+}
+
+vector<string> split_line(string str, char delimiter)
+{
+    istringstream iss(str);
+    string token;
+    vector<string> result;
+
+    while (std::getline(iss, token, delimiter))
+    {
+        result.push_back(token);
+    }
+
+    return result;
+}
+
+unordered_map<string, int> process_line(string str)
+{
+    unordered_map<string, int> result{
+        {"ID", -1},
+        {"red", 0},
+        {"green", 0},
+        {"blue", 0}};
+
+    vector<string> processed_line = split_line(str, ':');
+    result.at("ID") = std::stoi(split_line(processed_line.at(0), ' ').at(1));
+    vector<string> game_instances = split_line(processed_line.at(1), ';');
+    for (string &game : game_instances)
+    {
+        vector<string> game_result = split_line(game, ',');
+        for (string &color : game_result)
+        {
+            vector<string> color_result = split_line(color, ' ');
+            result.at(color_result.at(2)) = std::max(std::stoi(color_result.at(1)), result.at(color_result.at(2)));
+        }
+    }
+
+    return result;
+}
