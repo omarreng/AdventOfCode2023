4bf6447e794c732c6c1fb45f2ad7a5cb76f7f273
Oscar Manuel Marrero Engström <oscar.marrengo@gmail.com>
2023-12-12 20:06:03 +0100

    Day 3: Challenge 1 - Adds helper function to parse numbers
    Adds parse_numbers which is supposed to be called every time a
    numerical character is encountered while iterating over the input
    data.
    
    The function then parses the number it encounters and returns it
    together with the index of the the last character that belong to
    the number in the input data.

--- Day3/src/Day3_1.cpp
+++ Day3/src/Day3_1.cpp
@@ -7,11 +7,23 @@
 using namespace std;
 
 vector<vector<char>> process_input(vector<string> &vec);
+std::pair<int, int> parse_number(vector<char> &vec, int index);
 
 int main()
 {
     vector<string> input = readFile("input_day3.txt");
     vector<vector<char>> processed_input = process_input(input);
+
+    for (int i = 0; i < processed_input.size(); i++)
+    {
+        for (int j = 0; j < processed_input.at(i).size(); j++)
+        {
+            if (std::isdigit(processed_input.at(i).at(j)))
+            {
+                parse_number(processed_input.at(i), j);
+            }
+        }
+    }
 
     cout << "Hello from Day 3!" << endl;
 
@@ -31,4 +43,14 @@ vector<vector<char>> process_input(vector<string> &vec)
         result.push_back(temp_vec);
     }
     return result;
+}
+
+std::pair<int, int> parse_number(vector<char> &vec, int index)
+{
+    int result = 0;
+    while (index < vec.size() && std::isdigit(vec.at(index)))
+    {
+        result = result * 10 + vec.at(index++) - '0';
+    }
+    return std::make_pair(result, index);
 }
