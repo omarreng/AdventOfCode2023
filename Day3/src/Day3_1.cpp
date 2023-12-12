f360370e054d3f7213d31b0b4a306690b8c3dfdb
Oscar Manuel Marrero Engström <oscar.marrengo@gmail.com>
2023-12-12 18:43:39 +0100

    Day 3: Challenge 1 - Adds helper function to process input
    Adds the process_input function which further processes the read
    input from the .txt file. It processes the vector of strings to
    convert it into  a vector of vector of chars, which will be the
    format used to iterate over the data to solve the first challenge
    of day 3.

--- Day3/src/Day3_1.cpp
+++ Day3/src/Day3_1.cpp
@@ -6,7 +6,29 @@
 
 using namespace std;
 
+vector<vector<char>> process_input(vector<string> &vec);
+
 int main()
 {
+    vector<string> input = readFile("input_day3.txt");
+    vector<vector<char>> processed_input = process_input(input);
+
+    cout << "Hello from Day 3!" << endl;
+
     return 0;
+}
+
+vector<vector<char>> process_input(vector<string> &vec)
+{
+    vector<vector<char>> result;
+    for (string &str : vec)
+    {
+        vector<char> temp_vec;
+        for (char &c : str)
+        {
+            temp_vec.push_back(c);
+        }
+        result.push_back(temp_vec);
+    }
+    return result;
 }
