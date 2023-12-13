1c0b951d73b51845eba598796ff66dee6ca8b5b1
Oscar Manuel Marrero Engström <oscar.marrengo@gmail.com>
2023-12-13 21:44:33 +0100

    Day 4: Challenge 1 - Adds a .cpp file for the the first challenge

---
+++ Day4/src/day4_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "fileio.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from day 4!" << endl;
+
+    return 0;
+}
