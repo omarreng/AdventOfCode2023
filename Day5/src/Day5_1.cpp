6a77995818d87492262e19dba2cd36d2e51a8acf
Oscar <oscar.marrengo@gmail.com>
2023-12-16 11:43:32 +0100

    Day 5 - Adds .cpp files for both challenges

---
+++ Day5/src/Day5_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "fileio.hpp"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from day 5!" << endl;
+
+    return 0;
+}
