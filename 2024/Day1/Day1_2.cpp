9ad6b3e542b2c764b9c78538c1f2801b27c2f4bb
Oscar <oscar.marrengo@gmail.com>
2024-12-01 18:41:32 +0100

    Adds files for second part of Day 1

--- 2024/Day1/Day1_2.cpp
+++ 2024/Day1/Day1_2.cpp
@@ -6,7 +6,7 @@
 
 int main()
 {
-    auto input = read_file("Day1_1_input.txt"); 
+    auto input = read_file("Day1_input.txt"); 
 
     vector<int64_t> list_1, list_2;
     for (auto row : input)
@@ -16,15 +16,9 @@ int main()
         list_2.push_back(stoi(numbers[3]));
     }
 
-    sort(list_1.begin(), list_1.end());
-    sort(list_2.begin(), list_2.end());
+    
 
     int result = 0;
-
-    for (int i = 0; i < list_1.size(); i++)
-    {
-      result += std::abs(list_1.at(i) - list_2.at(i));
-    }
 
     cout << "The result is: " << result << endl;
 
