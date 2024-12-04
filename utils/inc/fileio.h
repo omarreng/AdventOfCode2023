3cb12005706b807563bd185bec24e2e39125fe0e
omarreng <oscar.marrengo@gmail.com>
2024-12-04 12:18:17 +0100

    Adds read_file_char to the utils/ folder

--- utils/inc/fileio.h
+++ utils/inc/fileio.h
@@ -4,6 +4,7 @@
 
 using namespace std;
 
-vector<string> read_file(string filePath);
+vector<string> read_file(string file_ath);
 vector<string> split_line(string str, char delimiter = ' ');
 vector<vector<string>> split_vector(const vector<string> &vec, string delimiter = "\n");
+vector<vector<char>> read_file_char(string file_path);
--- utils/src/fileio.cpp
+++ utils/src/fileio.cpp
@@ -9,61 +9,72 @@
 
 using namespace std;
 
-vector<string> read_file(string filePath)
+vector<string> read_file(string file_path)
 {
-    ifstream inputFile(filePath, ifstream::in);
+  ifstream inputFile(file_path, ifstream::in);
 
-    if (!inputFile.is_open())
-    {
-        ostringstream error_message;
-        error_message << "Error opening file: " << filePath;
-        throw invalid_argument(error_message.str());
-    }
+  if (!inputFile.is_open())
+  {
+    ostringstream error_message;
+    error_message << "Error opening file: " << file_path;
+    throw invalid_argument(error_message.str());
+  }
 
-    vector<string> result;
-    string line;
+  vector<string> result;
+  string line;
 
-    while (getline(inputFile, line))
-    {
-        result.push_back(line);
-    }
+  while (getline(inputFile, line))
+  {
+    result.push_back(line);
+  }
 
-    inputFile.close();
+  inputFile.close();
 
-    return result;
+  return result;
 }
 
 vector<string> split_line(string str, char delimiter)
 {
-    istringstream iss(str);
-    string token;
-    vector<string> result;
+  istringstream iss(str);
+  string token;
+  vector<string> result;
 
-    while (std::getline(iss, token, delimiter))
-    {
-        result.push_back(token);
-    }
+  while (std::getline(iss, token, delimiter))
+  {
+    result.push_back(token);
+  }
 
-    return result;
+  return result;
 }
 
 vector<vector<string>> split_vector(const vector<string> &vec, string delimiter)
 {
-    vector<vector<string>> result;
-    vector<string> temp;
-    for (string str : vec)
+  vector<vector<string>> result;
+  vector<string> temp;
+  for (string str : vec)
+  {
+    if (str == delimiter)
     {
-        if (str == delimiter)
-        {
-            result.push_back(temp);
-            temp.clear();
-            continue;
-        }
-        temp.push_back(str);
+      result.push_back(temp);
+      temp.clear();
+      continue;
     }
-    if (!temp.empty())
-    {
-        result.push_back(temp);
-    }
-    return result;
+    temp.push_back(str);
+  }
+  if (!temp.empty())
+  {
+    result.push_back(temp);
+  }
+  return result;
+}
+
+vector<vector<char>> read_file_char(string file_path)
+{
+  vector<vector<char>> result;
+  auto input = read_file(file_path);
+  for (auto line : input)
+  {
+    result.push_back(vector<char>(line.begin(), line.end()));
+  }
+  return result;
 }
