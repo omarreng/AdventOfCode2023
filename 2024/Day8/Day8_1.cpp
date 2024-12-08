e57e2ecea7a12712a37cd2fd3127b908907d29d2
omarreng <oscar.marrengo@gmail.com>
2024-12-09 00:20:43 +0100

    Solution for Day 8 Problem 1

---
+++ 2024/Day8/Day8_1.cpp
@@ -0,0 +1,109 @@
+#include <functional>
+#include <map>
+#include <set>
+
+#include "fileio.h"
+#include "strategies.h"
+
+map<char, vector<pair<int, int>>> scan_for_antennas(vector<vector<char>> &input);
+void process_antenna_pair(pair<int, int> a1, pair<int, int> a2, const vector<vector<char>> &input, set<pair<int, int>> &results);
+function<int(int)> get_linear_func(pair<int, int> p1, pair<int, int> p2);
+void print_map(const vector<vector<char>> &map);
+
+int main()
+{
+  auto input = read_file_char("Day8_input.txt");
+  auto antenna_map = scan_for_antennas(input);
+
+  print_map(input);
+
+  set<pair<int, int>> results;
+  for (const auto &[frequency, antennas] : antenna_map)
+  {
+    for (int i = 0; i < antennas.size(); i++)
+    {
+      for (int j = i + 1; j < antennas.size(); j++)
+      {
+        process_antenna_pair(antennas[i], antennas[j], input, results);
+      }
+    }
+  }
+
+  for (const auto &result : results)
+  {
+    if (input[result.second][result.first] == '.')
+    {
+      input[result.second][result.first] = '#';
+    }
+  }
+
+  print_map(input);
+
+  cout << "Result is: " << results.size() << endl;
+
+  return 0;
+}
+
+map<char, vector<pair<int, int>>> scan_for_antennas(vector<vector<char>> &input)
+{
+  map<char, vector<pair<int, int>>> result;
+  for (int row = 0; row < input.size(); row++)
+  {
+    for (int col = 0; col < input[0].size(); col++)
+    {
+      if (input[row][col] != '.')
+      {
+        result[input[row][col]].push_back({col, row});
+      }
+    }
+  }
+  return result;
+}
+
+void process_antenna_pair(pair<int, int> a1, pair<int, int> a2, const vector<vector<char>> &input, set<pair<int, int>> &results)
+{
+  const auto line = get_linear_func(a1, a2);
+  int dx = a2.first - a1.first;
+  int dy = a2.second - a1.second;
+
+  const auto antenna_1 = a1; // a1.first < a2.first ? a1 : a2;
+  const auto antenna_2 = a2; // a1.first < a2.first ? a2 : a1;
+
+  const auto possible_antinode_1 = make_pair(antenna_1.first - dx, antenna_1.second - dy);
+  const auto possible_antinode_2 = make_pair(antenna_2.first + dx, antenna_2.second + dy);
+
+  if (is_bound(input, possible_antinode_1.second, possible_antinode_1.first))
+  {
+    results.insert(possible_antinode_1);
+  }
+
+  if (is_bound(input, possible_antinode_2.second, possible_antinode_2.first))
+  {
+    results.insert(possible_antinode_2);
+  }
+}
+
+function<int(int)> get_linear_func(pair<int, int> p1, pair<int, int> p2)
+{
+  return [p1, p2](int x) -> int
+  {
+    int my = p2.second - p1.second;
+    int mx = p2.first - p1.first;
+    int by = p1.second * mx - my * p1.first;
+
+    return (my * x + by) / mx;
+  };
+}
+
+void print_map(const vector<vector<char>> &map)
+{
+  for (const auto row : map)
+  {
+    for (const auto col : row)
+    {
+      cout << col;
+    }
+    cout << endl;
+  }
+  cout << endl;
+}
