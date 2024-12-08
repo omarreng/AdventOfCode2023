40150b18a51ba1314b92cf77f6b37f9bdd7cb02e
omarreng <oscar.marrengo@gmail.com>
2024-12-09 00:56:57 +0100

    Solution for Day 8 Problem 2

--- 2024/Day8/Day8_2.cpp
+++ 2024/Day8/Day8_2.cpp
@@ -80,8 +80,8 @@ void process_antenna_pair(pair<int, int> a1, pair<int, int> a2, const vector<vec
     iteration++;
   } while (is_bound(input, possible_antinode_1.first, possible_antinode_1.second));
 
+  // Comment
   iteration = 0;
-
   do
   {
     possible_antinode_2 = make_pair(antenna_2.first + delta * iteration, line(antenna_2.first + delta * iteration));
