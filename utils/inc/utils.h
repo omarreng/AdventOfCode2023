9f20f9bd364863b9c2854942a7b346676fd2434c
Oscar Marrero Engström <oscar.marrengo@gmail.com>
2024-12-12 16:25:08 +0100

    Adds hash function for std::pair to utils

--- utils/inc/utils.h
+++ utils/inc/utils.h
@@ -10,3 +10,14 @@ bool is_bound(const vector<vector<T>> &vec, int ix, int iy)
 {
   return ix >= 0 && iy >= 0 && ix < vec.size() && iy < vec[0].size();
 }
+
+struct pair_hash
+{
+  template <typename T1, typename T2>
+  size_t operator()(const pair<T1, T2> &p) const
+  {
+    auto hash1 = hash<T1>{}(p.first);
+    auto hash2 = hash<T2>{}(p.second);
+    return hash1 ^ (hash2 << 1);
+  }
+};
